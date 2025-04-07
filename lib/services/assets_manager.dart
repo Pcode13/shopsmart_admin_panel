248cf24508a2affedc1835275beae9afbba51f3e
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-04-07 14:42:27 +0530

    Make the upload product screen dynamic to allow the admin to edit his product

--- lib/services/assets_manager.dart
+++ lib/services/assets_manager.dart
@@ -2,23 +2,13 @@ class AssetsManager {
   static String imagesPath = "assets/images";
   static String categoriesImagesPath = "assets/images/categories";
   static String dashboardImagesPath = "assets/images/dashboard";
+  static String bagImagesPath = "assets/images/dashboard";
 
-// general images
+  // general images
   static String warning = "$imagesPath/warning.png";
   static String error = "$imagesPath/error.png";
-  static String emptySearch = "$imagesPath/empty_search.png";
-  static String shoppingCart = "$imagesPath/shopping_cart.png";
 
-  // Categories path
-  static String mobiles = "$categoriesImagesPath/mobiles.png";
-  static String fashion = "$categoriesImagesPath/fashion.png";
-  static String watch = "$categoriesImagesPath/watch.png";
-  static String book = "$categoriesImagesPath/book_img.png";
-  static String electronics = "$categoriesImagesPath/electronics.png";
-  static String cosmetics = "$categoriesImagesPath/cosmetics.png";
-  static String shoes = "$categoriesImagesPath/shoes.png";
-  static String pc = "$categoriesImagesPath/pc.png";
-
-  static String order = "$dashboardImagesPath/order.png";
-  static String cloud = "$dashboardImagesPath/cloud.png";
+  static String cloud = "$bagImagesPath/bag_wish.png";
+  static String order = "$bagImagesPath/order.png";
+  static String shoppingCart = "$bagImagesPath/shopping_cart.png";
 }
